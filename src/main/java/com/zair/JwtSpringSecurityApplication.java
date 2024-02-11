0c3bedd13b859dfdd672775b9f982427b814e0d9
ZairBulos <buloszair@gmail.com>
2024-02-11 12:45:50 -0300

    Initial project

---
+++ src/main/java/com/zair/JwtSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.zair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zair/JwtSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
